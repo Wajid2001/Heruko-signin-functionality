f8c6231c8baa85dabc0183d907d80593949dc828
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-07-08 05:54:54 +0530

    Fixed an issue

--- React_Files/App.jsx
+++ React_Files/App.jsx
@@ -21,7 +21,7 @@ if (document.cookie && document.cookie !== "") {
 	for (let i = 0; i < cookies.length; i++) {
 		const cookie = cookies[i].trim();
 		// Does this cookie string begin with the name we want?
-		if (cookie.substring(0, "csrftoken".length + 1) === name + "=") {
+		if (cookie.substring(0, "csrftoken".length + 1) === "csrftoken" + "=") {
 			cookieValue = decodeURIComponent(cookie.substring("csrftoken".length + 1));
 			break;
 		}
